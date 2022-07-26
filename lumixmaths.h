153e0719358f9ceed3eee8c0d5d989a508818c4b
LumixCuck <97337811+LumixCuck@users.noreply.github.com>
2022-07-26 22:14:31 +0200

    Add files via upload

---
+++ lumixmaths.h
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+#include <stdio.h>
+int addition(int nombre1, int nombre2, int resultat);
+int division(int nombre1, int nombre2, int resultat);
+int modulo(int nombre1, int nombre2, int resultat);
+int soustraction(int nombre1, int nombre2, int resultat);
+int multiplication(int nombre1, int nombre2, int resultat);
